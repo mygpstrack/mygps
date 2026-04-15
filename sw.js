f2399b7676e2cbfac2af1ef27132b8b54e178a60
mygpstrack <116297236+mygpstrack@users.noreply.github.com>
2026-04-15 15:45:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "mygps-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      "./admin.html",
+      "./live.html",
+      "./manifest.webmanifest"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
